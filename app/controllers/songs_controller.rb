672930cf07bd9603524d2cc1ec3fb8325643a41f
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-19 11:34:45 -0600

    commit tree 4ddc2918623d87b02fbd22cb7db89e1c9142c3db

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
     end
 
     def create
-        @song = Song.new(song_params(:name, :artist_id, :genre_id))
+        @song = Song.new(song_params)
         @song.save
         redirect_to song_path(@song)
     end
@@ -30,8 +30,8 @@ class SongsController < ApplicationController
 
     private
 
-    def song_params(*args)
-        params.require(:song).permit(*args)
+    def song_params
+        params.require(:song).permit(:name, :artist_id, :genre_id)
     end
     
 end
